275977afbe43e5c651e95152938db29dc76beb6d
pcrepairguy510@gmail.com <yea@sfusd.edu>
2017-08-10 11:25:53 -0700

    Added HTML for site

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
 from .models import Post
 
-# to make the model visible on the admin page
+# to make the model visible on the admin page410
 admin.site.register(Post)
